322039fbe24280b22edd6f3b3823dfe4ae80e3d4
marta <mmmtnez@cnb.csic.es>
2019-02-01 07:33:42 +0100

    Replacement of version 1.13 by 1.14

--- phenix/constants.py
+++ phenix/constants.py
@@ -25,7 +25,7 @@
 # **************************************************************************
 PHENIX_HOME = 'PHENIX_HOME'
 PHENIXVERSIONFILENAME = './phenix_env.sh'
-PHENIXVERSION = '1.13'
+PHENIXVERSION = '1.14'
 
 #python used to run phenix scripts
 PHENIX_PYTHON = 'phenix.python '  # keep the ending space
