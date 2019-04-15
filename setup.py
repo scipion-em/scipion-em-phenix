bd713d927d335c2637a78ab07fc4670897084284
Marta Martinez <mmmtnez@users.noreply.github.com>
2019-04-15 14:54:29 +0200

    Update setup.py with version 1.0.3

--- setup.py
+++ setup.py
@@ -40,12 +40,12 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.1',  # Required
+    version='1.0.3',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='Phenix ready to use in scipion.',  # Required
+    description='PHENIX programs in Scipion. Warning: Install PHENIX v. 1-13-2998',  # Required
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
